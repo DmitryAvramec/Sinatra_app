ee68b88f6c10145c49af5e611f7870cd95d2da42
Dmitry Avramets <dmitry.avramec@gmail.com>
2017-08-01 09:42:40 +0300

    Rename encode/decode methods

--- advanced.rb
+++ advanced.rb
@@ -13,17 +13,8 @@ require 'jwt'
 
 signing_key_path = File.expand_path("../app.rsa", __FILE__)
 verify_key_path = File.expand_path("../app.rsa.pub", __FILE__)
-
-rsa_private = ""
-rsa_public = ""
-
-File.open(signing_key_path) do |file|
-  rsa_private = OpenSSL::PKey.read(file)
-end
-
-File.open(verify_key_path) do |file|
-  rsa_public = OpenSSL::PKey.read(file)
-end
+rsa_private = OpenSSL::PKey.read(File.open(signing_key_path))
+rsa_public = OpenSSL::PKey.read(File.open(verify_key_path))
 
 set :rsa_private, rsa_private
 set :rsa_public, rsa_public
@@ -44,33 +35,29 @@ use OmniAuth::Builder do
 end
 
 helpers do
-  def my_encode text
+  def cript text
     JWT.encode text.to_s, settings.rsa_private, 'RS256'
   end
 
-  def my_decode text
+  def decript text
     JWT.decode(text, settings.rsa_public, true, { :algorithm => 'RS256' })[0].to_i
   end
 
   def protected!
-    return unless authorized?
+    return unless session['token']
     redirect '/login'
   end
 
-  def authorized?
-    session['token'].nil?
-  end
-
   def create_session user_id, token
-    session['user_count'] = my_encode('0')
-    session['number'] = my_encode('0')
-    session['user'] = my_encode(user_id.to_s)
-    session['token'] = my_encode(token.to_s)
+    session['user_count'] = cript('0')
+    session['number'] = cript('0')
+    session['user'] = cript(user_id.to_s)
+    session['token'] = cript(token.to_s)
   end
 
   def refresh_step
-    session['user_count'] = my_encode('0')
-    session['number'] = my_encode('0')
+    session['user_count'] = cript('0')
+    session['number'] = cript('0')
   end
 
   def destroy_session
@@ -100,9 +87,9 @@ get '/auth/:provider/callback' do
 end
 
 get '/profile' do
-  current_user_id = my_decode(session['user'])
+  current_user_id = decript(session['user'])
   current_user = User.find(current_user_id)
-  current_count = my_decode(session['user_count'])
+  current_count = decript(session['user_count'])
   current_user.update(count: current_count) if current_count > current_user.count
   sort_users = User.order(count: :desc)
   refresh_step
@@ -110,27 +97,27 @@ get '/profile' do
 end
 
 post '/submit' do
-  current_step = my_decode(session['number'])
+  current_step = decript(session['number'])
   result = DATA[current_step]
   right_answer = result["right_answer"]
-  current_count = my_decode(session['user_count'])
+  current_count = decript(session['user_count'])
   current_count += 1 if params[:user_input] == right_answer
   current_step += 1
-  session['number'] = my_encode(current_step)
-  session['user_count'] = my_encode(current_count)
+  session['number'] = cript(current_step)
+  session['user_count'] = cript(current_count)
   redirect '/profile' if current_step == DATA.size   
   redirect'/step'
 end
 
 get '/step' do
   protected!
-  current_step = my_decode(session['number'])
+  current_step = decript(session['number'])
   result = DATA[current_step]
   question = result["question"]
   erb :step1, locals: { question: question, number: current_step }
 end
 
 get '/logout' do
-    destroy_session
-    redirect '/'
+  destroy_session
+  redirect '/'
 end
